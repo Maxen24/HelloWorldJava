05d550a9661a4dc07f507cbebb8080b74e73d6d2
Maxen24 <mshubham242@gmail.com>
2022-09-05 10:12:32 +0530

    Added Welcome File

---
+++ Welcome.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+/**
+ * 
+ */
+
+/**
+ * @author mshub
+ *
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("Hello World!");
+
+	}
+
+}
